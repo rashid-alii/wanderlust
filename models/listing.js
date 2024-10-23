11edd76332abb5817b4ba93721ec5aa3a40f8b3f
Rashid Ali <161059307+rashid-alii@users.noreply.github.com>
2024-10-23 21:34:10 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -2,25 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-  title:{
+  title: {
     type: String,
     required: true,
-
   },
   description: String,
-  images:{
-    type: String,
-    default: "https://images.unsplash.com/photo-1704664265297-b1410ae6c6f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-
-    set: (v) => v == ""? "https://images.unsplash.com/photo-1704664265297-b1410ae6c6f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
-
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage",
+    },
+    url: {
+      type: String,
+      default: "https://images.unsplash.com/photo-1704664265297-b1410ae6c6f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      set: (v) => (v === "" ? "https://images.unsplash.com/photo-1704664265297-b1410ae6c6f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v),
+    },
   },
   price: Number,
   location: String,
   country: String,
 });
-
-
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
