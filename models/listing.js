092f026875a21d9430c9342ef9db8045cfae1a23
Rashid Ali <161059307+rashid-alii@users.noreply.github.com>
2024-11-02 14:39:03 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -14,15 +14,18 @@ const listingSchema = new Schema({
     },
     url: {
       type: String,
-      default: "https://images.unsplash.com/photo-1704664265297-b1410ae6c6f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      set: (v) => (v === "" ? "https://images.unsplash.com/photo-1704664265297-b1410ae6c6f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v),
+      default:
+        "https://images.unsplash.com/photo-1704664265297-b1410ae6c6f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      set: (v) =>
+        v === ""
+          ? "https://images.unsplash.com/photo-1704664265297-b1410ae6c6f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          : v,
     },
   },
   price: Number,
   location: String,
   country: String,
 });
-
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
