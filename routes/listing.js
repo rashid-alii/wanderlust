7cd68f9f49377705cd13bc731cdb9c0b6ffd8ba7
Rashid Ali <161059307+rashid-alii@users.noreply.github.com>
2024-11-16 11:34:49 +0530

    Create listing.js

---
+++ routes/listing.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+
+const listingController = require("../controllers/listings.js");
+
+//index route
+//create route
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
+
+//Show Route
+// Update Route
+// delete route
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
+//New Route
+router.get("/new", isLoggedIn, listingController.randerNewForm);
+
+// Edit Route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.randerEditForm)
+);
+
+module.exports = router;
