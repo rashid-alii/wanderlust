3237a6c59ead69f447192f0d4fcfcb30b2e5d020
Rashid Ali <161059307+rashid-alii@users.noreply.github.com>
2024-11-16 11:29:29 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({ ...obj, owner: "67339ce3d104ed1bc3407a36" }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
