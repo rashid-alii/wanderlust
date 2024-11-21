e7955b4ad9c204cd18f6c2f3ca20c639e55ad8bd
Rashid Ali <161059307+rashid-alii@users.noreply.github.com>
2024-11-21 19:51:27 +0530

    Update script.js

--- public/JS/script.js
+++ public/JS/script.js
@@ -2,7 +2,7 @@
 (() => {
   'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to.
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll('.needs-validation')
 
   // Loop over them and prevent submission
