1e2cd64f13663bb143dfeee552dc76f8a20d0aa9
Rashid Ali <161059307+rashid-alii@users.noreply.github.com>
2024-11-21 19:52:15 +0530

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -2,18 +2,19 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
+
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
+
+const reviewController = require("../controllers/reviews.js");
 const {
   validateReview,
   isLoggedIn,
   isReviewAuthor,
 } = require("../middleware.js");
 
-const reviewController = require("../controllers/reviews.js");
-const review = require("../models/review.js");
-
-//Post Review Route
+//REVIEWS
+//Post route
 
 router.post(
   "/",
@@ -22,7 +23,7 @@ router.post(
   wrapAsync(reviewController.createReview)
 );
 
-// Delete Review Route
+// Delete review route
 router.delete(
   "/:reviewId",
   isLoggedIn,
