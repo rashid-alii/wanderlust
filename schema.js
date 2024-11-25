6025ed63176db7170db205570e8c3808873c6947
Rashid Ali <161059307+rashid-alii@users.noreply.github.com>
2024-11-25 21:43:54 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -9,10 +9,8 @@ module.exports.listingSchema = Joi.object({
         location: Joi.string().required(),
         country: Joi.string().required(),
         price: Joi.number().required().min(0),
-        image: Joi.object({
-            filename: Joi.string().optional(),
-            url: Joi.string().allow("", null),
-        }).optional(),
+        image: Joi.string().optional(),
+        category: Joi.string().optional(),
     }).required()
 });
 
